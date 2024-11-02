9022a05887a12cc1b11656d86eca361dd2578910
Jeremy Davis <jeremyrdavis@mac.com>
2024-11-02 14:29:06 -0400

    button working

---
+++ src/main/webui/src/components/Register.jsx
@@ -0,0 +1,7 @@
+import {Button, DarkThemeToggle} from "flowbite-react";
+
+export function Register() {
+    return (
+            <Button onClick={() => console.log("Register!") }>Register</Button>
+    );
+}
