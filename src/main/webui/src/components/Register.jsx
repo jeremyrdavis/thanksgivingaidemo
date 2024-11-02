44661200ea2ac6308b1b14f7aa426a3d7dc7646e
Jeremy Davis <jeremyrdavis@mac.com>
2024-11-02 15:37:00 -0400

    cleaned up register form

--- src/main/webui/src/components/Register.jsx
+++ src/main/webui/src/components/Register.jsx
@@ -1,7 +1,21 @@
-import {Button, DarkThemeToggle} from "flowbite-react";
+import {Button, Label, TextInput} from "flowbite-react";
 
 export function Register() {
     return (
-            <Button onClick={() => console.log("Register!") }>Register</Button>
+        <div>
+                <form id="registerform" className="flex max-w-md flex-col gap-4"
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        console.log(e.target.email.value);
+                    }}>
+                    <div>
+                        <div className="mb-2 block">
+                            <Label htmlFor="email" value="Enter your email to get started"/>
+                        </div>
+                        <TextInput id="email" type="email" placeholder="name@flowbite.com" required />
+                    </div>
+                    <Button type="submit">Register</Button>
+            </form>
+        </div>
     );
 }
